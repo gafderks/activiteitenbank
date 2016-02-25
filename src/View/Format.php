31724348cd4da043768b679f41e7043f96a17c11
Geert Derks <g.a.f.derks@student.tue.nl>
2016-02-25 20:20:29 +0100

    Added PHPDoc

--- src/View/Format.php
+++ src/View/Format.php
@@ -2,9 +2,21 @@
 
 namespace View;
 
+/**
+ * Class Format
+ * Used as a view helper.
+ *
+ * @package View
+ */
 class Format
 {
 
+    /**
+     * Converts a float to a formatted euro value.
+     *
+     * @param $float float float to convert
+     * @return string formatted euro string
+     */
     public function float2Euro($float) {
         $euros = floor($float);
         $cents = str_pad(($float-$euros)*100, 2, '0', STR_PAD_LEFT);
@@ -12,6 +24,12 @@ class Format
         return "&euro;&nbsp;".$euros.".".$cents;
     }
 
+    /**
+     * Converts an integer containing the number of minutes to a formatted time.
+     *
+     * @param $int integer minutes
+     * @return string formatted hours and minutes
+     */
     public function int2Time($int) {
         $hours   = floor($int/60);
         $minutes = str_pad($int%60, 2, '0', STR_PAD_LEFT);
@@ -19,11 +37,19 @@ class Format
         return $hours.":".$minutes;
     }
 
+    /**
+     * Converts BBCode to HTML using the SBBCodeParser Library.
+     *
+     * @param $bbcode string input BBCode
+     * @return string converted HTML code
+     * @throws \SBBCodeParser\Exception_MissingEndTag
+     */
     public function bb2Html($bbcode) {
         $parser = new \SBBCodeParser\Node_Container_Document();
 
         $app = \Slim\Slim::getInstance();
 
+        // locate smileys folder
         $sF = $app->config['componentsUrl'].'/ckeditor/plugins/smiley/images/';
 
         $parser->add_emoticons([
