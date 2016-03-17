d07588f241ebaf64129bb8633223ae35b6561476
Geert Derks <g.a.f.derks@student.tue.nl>
2016-03-17 02:18:55 +0100

    Let generate-cache.php include bootstrap.php instead of doing it all on its own.

--- generate-cache.php
+++ generate-cache.php
@@ -6,43 +6,24 @@
  * @see http://twig.sensiolabs.org/doc/extensions/i18n.html#extracting-template-strings
  */
 
-// load required files
-require_once "vendor/autoload.php";
-$config = include("config.php");
+/********************************************************************************
+ * Bootstrap
+ *******************************************************************************/
 
-// create autoloader
-function __autoload($className) {
-    include __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . str_replace('\\', '/', $className) . '.php';
-}
-spl_autoload_register('__autoload');
+include_once "bootstrap.php";
 
-\Slim\Slim::registerAutoloader();
+/********************************************************************************
+ * Iterate over all templates
+ *******************************************************************************/
 
-$tplDir = dirname(__FILE__)."/view/{$config['template']}/templates";
-$tmpDir = dirname(__FILE__)."/view/{$config['template']}/cache";
-$loader = new \Twig_Loader_Filesystem($tplDir);
+$templatesDirectory = dirname(__FILE__)."/view/{$config['template']}/templates";
 
-// force auto-reload to always have the latest version of the template
-$twig = new \Twig_Environment($loader, array(
-    'cache' => $tmpDir,
-    'auto_reload' => true
-));
-$twig->addExtension(new \Twig_Extensions_Extension_I18n());
-$twig->addExtension(new \Slim\Views\TwigExtension());
-// register custom filters
-$int2time = new Twig_SimpleFilter('int2time', ['\View\Format', 'int2Time']);
-$float2euro = new Twig_SimpleFilter('float2euro', ['\View\Format', 'float2Euro']);
-$bb2html = new Twig_SimpleFilter('bb2Html', ['\View\Format', 'bb2Html']);
-$twig->addFilter($int2time);
-$twig->addFilter($float2euro);
-$twig->addFilter($bb2html);
-// configure Twig the way you want
-
-// iterate over all your templates
-foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($tplDir), \RecursiveIteratorIterator::LEAVES_ONLY) as $file)
+foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($templatesDirectory),
+    \RecursiveIteratorIterator::LEAVES_ONLY) as $file)
 {
     // force compilation
     if ($file->isFile()) {
-        $twig->loadTemplate(str_replace($tplDir.DIRECTORY_SEPARATOR, '', $file));
+        $container['view']->getEnvironment()->loadTemplate(
+            str_replace($templatesDirectory.DIRECTORY_SEPARATOR, '', $file));
     }
 }
